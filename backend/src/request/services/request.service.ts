24cd111896ed458541568593ca6c103fd0d884f1
hheimerd <hheimerd@yandex.ru>
2021-07-17 18:17:02 +0300

    request helper added

---
+++ backend/src/request/services/request.service.ts
@@ -0,0 +1,13 @@
+import { ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export class RequestService {
+  getRequest(context: ExecutionContext) {
+    if (context.getType().toString() === 'graphql') {
+      const { req } = GqlExecutionContext.create(context).getContext();
+      return req;
+    } else {
+      return context.switchToHttp().getRequest();
+    }
+  }
+}
