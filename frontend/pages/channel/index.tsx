d83140a44168dda81bda913e676925fb0703c16e
Artur Nasyrov <sun604@gmail.com>
2021-07-22 09:09:26 +0300

    feat(channel): add channel edit page

--- frontend/pages/channel/index.tsx
+++ frontend/pages/channel/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -9,6 +9,7 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import {Htag} from "../../components";
+import {useRouter} from "next/router";
 
 const useStyles = makeStyles({
   table: {
@@ -20,30 +21,44 @@ const useStyles = makeStyles({
 });
 
 interface Row {
+  id: number,
   name: string,
   type: string,
   users_count: number,
   owner: string
+  role: string
 }
 
 const createData = (
+  id: number,
   name: string,
   type: string,
   users_count: number,
-  owner: string): Row => {
-  return { name, type, users_count, owner };
+  owner: string,
+  role: string
+): Row => {
+  return { id, name, type, users_count, owner, role };
 };
 
-const rows: Array<Row> = [
-  createData("Frozen yoghurt", "Private", 6, "Andrey"),
-  createData("Ice cream sandwich", "Private", 9, "Sergey"),
-  createData("Eclair", "Public", 16, "Alexandr"),
-  createData("Cupcake", "Public", 3, "Mihail"),
-  createData("Gingerbread", "Protected", 16, "Stepan")
-];
+const fetchChannels = (): Array<Row> => {
+  const rows: Array<Row> = [
+    createData(1, "Frozen yoghurt", "Private", 6, "me", "owner"),
+    createData(2, "Ice cream sandwich", "Private", 9, "me", "owner"),
+    createData(3, "Eclair", "Public", 16, "Alexandr", "user"),
+    createData(4, "Cupcake", "Public", 3, "Mihail", "user"),
+    createData(5, "Gingerbread", "Protected", 16, "Stepan", "admin")
+  ];
+  return rows;
+};
 
 const Channel = (): JSX.Element => {
   const classes = useStyles();
+  const router = useRouter();
+  const [rows, setRows] = useState<Array<Row>>();
+
+  useEffect(() => {
+    setRows(fetchChannels());
+  }, []);
 
   const handleLeave = (values: Row) => {
     console.log("The Values that you wish to leave ", values);
@@ -51,7 +66,10 @@ const Channel = (): JSX.Element => {
 
   const handleEdit = (values: Row) => {
     console.log("The Values that you wish to edit ", values);
+    router.push('/channel/edit/' + values.id);
   };
+
+  if (!rows) return null;
 
   return (
     <>
@@ -71,17 +89,18 @@ const Channel = (): JSX.Element => {
             {rows.map(row => (
               <TableRow key={row.name}>
                 <TableCell component="th" scope="row">
-                  <a href="#" className={classes.a}>{row.name}</a>
+                  <a href={"/channel/room/" + row.id} className={classes.a}>{row.name}</a>
                 </TableCell>
                 <TableCell>{row.type}</TableCell>
                 <TableCell>{row.users_count}</TableCell>
                 <TableCell>{row.owner}</TableCell>
                 <TableCell align="right">
+                  { row.role === "owner" 
+                    ? <Button aria-label="edit" onClick={() => handleEdit(row)}>Edit</Button>
+                    : null
+                  }
                   <Button aria-label="leave" onClick={() => handleLeave(row)}>
                     Leave
-                  </Button>
-                  <Button aria-label="edit" onClick={() => handleEdit(row)}>
-                    Edit
                   </Button>
                 </TableCell>
               </TableRow>
