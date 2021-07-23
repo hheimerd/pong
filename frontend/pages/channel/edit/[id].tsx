a0d3342a2d37090752c6cb85be54dbb485e37b2a
Artur Nasyrov <sun604@gmail.com>
2021-07-23 21:53:28 +0300

    feat(channel): add edit form submit handling

--- frontend/pages/channel/edit/[id].tsx
+++ frontend/pages/channel/edit/[id].tsx
@@ -7,203 +7,149 @@ import {FormControlLabel, Switch} from '@material-ui/core';
 import {Button, Htag} from '../../../components';
 import styles from './edit.module.css';
 
-interface Films {
-  title: string;
-  year: number;
+interface Users {
+  name: string;
+  id: number;
 }
 
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const top100Films: Array<Films> = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  { title: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001 },
-  { title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  { title: 'The Lord of the Rings: The Two Towers', year: 2002 },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  { title: 'Star Wars: Episode IV - A New Hope', year: 1977 },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-  { title: 'Casablanca', year: 1942 },
-  { title: 'City Lights', year: 1931 },
-  { title: 'Psycho', year: 1960 },
-  { title: 'The Green Mile', year: 1999 },
-  { title: 'The Intouchables', year: 2011 },
-  { title: 'Modern Times', year: 1936 },
-  { title: 'Raiders of the Lost Ark', year: 1981 },
-  { title: 'Rear Window', year: 1954 },
-  { title: 'The Pianist', year: 2002 },
-  { title: 'The Departed', year: 2006 },
-  { title: 'Terminator 2: Judgment Day', year: 1991 },
-  { title: 'Back to the Future', year: 1985 },
-  { title: 'Whiplash', year: 2014 },
-  { title: 'Gladiator', year: 2000 },
-  { title: 'Memento', year: 2000 },
-  { title: 'The Prestige', year: 2006 },
-  { title: 'The Lion King', year: 1994 },
-  { title: 'Apocalypse Now', year: 1979 },
-  { title: 'Alien', year: 1979 },
-  { title: 'Sunset Boulevard', year: 1950 },
-  { title: 'Dr. Strangelove', year: 1964 },
-  { title: 'The Great Dictator', year: 1940 },
-  { title: 'Cinema Paradiso', year: 1988 },
-  { title: 'The Lives of Others', year: 2006 },
-  { title: 'Grave of the Fireflies', year: 1988 },
-  { title: 'Paths of Glory', year: 1957 },
-  { title: 'Django Unchained', year: 2012 },
-  { title: 'The Shining', year: 1980 },
-  { title: 'WALL·E', year: 2008 },
-  { title: 'American Beauty', year: 1999 },
-  { title: 'The Dark Knight Rises', year: 2012 },
-  { title: 'Princess Mononoke', year: 1997 },
-  { title: 'Aliens', year: 1986 },
-  { title: 'Oldboy', year: 2003 },
-  { title: 'Once Upon a Time in America', year: 1984 },
-  { title: 'Witness for the Prosecution', year: 1957 },
-  { title: 'Das Boot', year: 1981 },
-  { title: 'Citizen Kane', year: 1941 },
-  { title: 'North by Northwest', year: 1959 },
-  { title: 'Vertigo', year: 1958 },
-  { title: 'Star Wars: Episode VI - Return of the Jedi', year: 1983 },
-  { title: 'Reservoir Dogs', year: 1992 },
-  { title: 'Braveheart', year: 1995 },
-  { title: 'M', year: 1931 },
-  { title: 'Requiem for a Dream', year: 2000 },
-  { title: 'Amélie', year: 2001 },
-  { title: 'A Clockwork Orange', year: 1971 },
-  { title: 'Like Stars on Earth', year: 2007 },
-  { title: 'Taxi Driver', year: 1976 },
-  { title: 'Lawrence of Arabia', year: 1962 },
-  { title: 'Double Indemnity', year: 1944 },
-  { title: 'Eternal Sunshine of the Spotless Mind', year: 2004 },
-  { title: 'Amadeus', year: 1984 },
-  { title: 'To Kill a Mockingbird', year: 1962 },
-  { title: 'Toy Story 3', year: 2010 },
-  { title: 'Logan', year: 2017 },
-  { title: 'Full Metal Jacket', year: 1987 },
-  { title: 'Dangal', year: 2016 },
-  { title: 'The Sting', year: 1973 },
-  { title: '2001: A Space Odyssey', year: 1968 },
-  { title: "Singin' in the Rain", year: 1952 },
-  { title: 'Toy Story', year: 1995 },
-  { title: 'Bicycle Thieves', year: 1948 },
-  { title: 'The Kid', year: 1921 },
-  { title: 'Inglourious Basterds', year: 2009 },
-  { title: 'Snatch', year: 2000 },
-  { title: '3 Idiots', year: 2009 },
-  { title: 'Monty Python and the Holy Grail', year: 1975 },
+const usersArr: Array<Users> = [
+  { name: 'Anna',       id: 1 },
+  { name: 'Andrey',     id: 2 },
+  { name: 'Pavel',      id: 3 },
+  { name: 'Ekaterina',  id: 4 },
+  { name: 'Michael',    id: 5 },
+  { name: 'Boris',      id: 6 },
+  { name: 'Elena',      id: 7 },
 ];
 
 const ChannelRoom = (): JSX.Element => {
   const router = useRouter();
+  // channel id
   const { id } = router.query;
-  // autocomplete
-  const [value, setValue] = React.useState<Array<Films>>([top100Films[13]]);
-  // switch
-  const [state, setState] = React.useState(false);
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setState(event.target.checked);
-  };
-  useEffect(() => {
-    console.log('switch checkedA: ' + state);
-  }, [state]);
+  // form fields
+  const [usersValue, setUsersValue] = React.useState<Array<Users>>([usersArr[3], usersArr[5]]);
+  const [adminsValue, setAdminsValue] = React.useState<Array<Users>>([usersArr[1]]);
+  const [isPrivate, setPrivate] = React.useState(false);
+  const [name, setName] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-  // rounter
+  const fetchInitialFormData = () => {
+    return 'Artur';
+  };
+
+  useEffect(() => {
+    setName(fetchInitialFormData);
+  }, []);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    console.log(name + ", " + usersValue.join(", ") + ", " + adminsValue.join(", ") + ", " + isPrivate + ", " + password);
+  };
+
+  // check router variable type
   if (typeof id !== "string") return null;
 
   return (
     <div className={styles.form}>
       <Htag tag='h1'>Edit channel id: {id}</Htag>
-      <div className={styles.line}>
-        <TextField 
-          id="outlined-size-normal" 
-          label="Name" 
-          fullWidth 
-          size="small" 
-          variant="outlined" 
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-      </div>
-      <div className={styles.line}>
-        <Autocomplete
-          fullWidth
-          multiple
-          id="fixed-tags-demo"
-          value={value}
-          onChange={(_event: React.ChangeEvent, newValue: Films[]) => {
-            setValue([
-              ...newValue,
-            ]);
-          }}
-          options={top100Films}
-          getOptionLabel={(option) => option.title}
-          getOptionSelected={(option, value) => option.title === value.title}
-          renderTags={(tagValue, getTagProps) =>
-            tagValue.map((option, index) => (
-              <Chip
-                label={option.title}
-                {...getTagProps({ index })}
+      <form onSubmit={handleSubmit}>
+        <div className={styles.line}>
+          <TextField 
+            id="outlined-size-normal" 
+            label="Name" 
+            fullWidth 
+            size="small" 
+            variant="outlined"
+            onChange={event => setName(event.target.value)} 
+            InputLabelProps={{
+              shrink: true,
+            }}
+            defaultValue={name}
+          />
+        </div>
+        <div className={styles.line}>
+          <Autocomplete fullWidth multiple id="usersArr-tags" value={usersValue}
+            onChange={(_event: React.ChangeEvent, newValue: Users[]) => {
+              setUsersValue([
+                ...newValue,
+              ]);
+            }}
+            options={usersArr}
+            getOptionLabel={(option) => option.name}
+            getOptionSelected={(option, value) => option.name === value.name}
+            renderTags={(tagValue, getTagProps) =>
+              tagValue.map((option, index) => (
+                <Chip label={option.name} {...getTagProps({ index })} />
+              ))
+            }
+            renderInput={(params) => (
+              <TextField{...params}
+                label="Users"
+                placeholder="Add user" 
+                variant="outlined" 
+                InputLabelProps={{
+                  shrink: true,
+                }}
               />
-            ))
-          }
-          renderInput={(params) => (
-            <TextField {...params}
-              label="Users"
-              placeholder="Add user" 
-              variant="outlined" 
-              InputLabelProps={{
-                shrink: true,
-              }}
+            )}
+          />
+        </div>
+        <div className={styles.line}>
+          <Autocomplete fullWidth multiple id="admins-tags" value={adminsValue}
+            onChange={(_event: React.ChangeEvent, newValue: Users[]) => {
+              setAdminsValue([
+                ...newValue,
+              ]);
+            }}
+            options={usersArr}
+            getOptionLabel={(option) => option.name}
+            getOptionSelected={(option, value) => option.name === value.name}
+            renderTags={(tagValue, getTagProps) =>
+              tagValue.map((option, index) => (
+                <Chip label={option.name} {...getTagProps({ index })} />
+              ))
+            }
+            renderInput={(params) => (
+              <TextField{...params}
+                label="Admins"
+                placeholder="Add admin" 
+                variant="outlined" 
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            )}
+          />
+        </div>
+        <div className={styles.line}>
+          <FormControlLabel 
+            control={
+              <Switch 
+                checked={isPrivate}
+                onChange={event => setPrivate(event.target.checked)} 
+                name="checkedA" 
+                color="primary"
+              />
+            }
+            label="Private" />
+        </div>
+        { isPrivate ? 
+          <div className={styles.line}>
+            <TextField id="outlined-size-normal2" label="Password" fullWidth 
+              size="small"
+              type="password"
+              variant="outlined"
+              InputLabelProps={{ shrink: true, }}
+              onChange={event => setPassword(event.target.value)}
             />
-          )}
-        />
-      </div>
-      <div className={styles.line}>
-        <FormControlLabel 
-          control={
-            <Switch 
-              checked={state}
-              onChange={handleChange} 
-              name="checkedA" 
-            />
-          }
-          label="Private" />
-      </div>
-      <div className={styles.line}>
-        <TextField id="outlined-size-normal2" label="Password" fullWidth 
-          size="small"
-          type="password"
-          variant="outlined"
-          InputLabelProps={{ shrink: true, }}/>
-      </div>
-      <div className={styles.line}>
-        <Button appearance="primary"> Submit </Button>
-      </div>
-    </div>
+          </div>
+          : null 
+        }
+        <div className={styles.line}>
+          <Button appearance="primary">Submit</Button>
+        </div>
+      </form>
+    </ div>
   );
 };
 
