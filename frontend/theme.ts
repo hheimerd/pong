f9adcded3c0dfacdc56e436449d511e65af074f9
Artur Nasyrov <sun604@gmail.com>
2021-07-31 18:34:32 +0300

    fix: rename createTheme to createMuiTheme

--- frontend/theme.ts
+++ frontend/theme.ts
@@ -1,21 +1,21 @@
-import { red } from '@material-ui/core/colors';
-import {createTheme} from '@material-ui/core/styles';
+import { red } from "@material-ui/core/colors";
+import { createMuiTheme } from "@material-ui/core/styles";
 
 // Create a theme instance.
-const theme = createTheme({
+const theme = createMuiTheme({
   palette: {
-    type: 'dark',
+    type: "dark",
     primary: {
-      main: '#FFB886',
+      main: "#FFB886",
     },
     secondary: {
-      main: '#19857b',
+      main: "#19857b",
     },
     error: {
       main: red.A400,
     },
     background: {
-      default: '#057A68',
+      default: "#057A68",
     },
   },
 });
