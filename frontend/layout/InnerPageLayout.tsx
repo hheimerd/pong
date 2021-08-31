d5bf32c23a0f2841cffc016c1e12f5ce8685855a
Artur Nasyrov <sun604@gmail.com>
2021-08-31 10:57:17 +0300

    add nav styling

--- frontend/layout/InnerPageLayout.tsx
+++ frontend/layout/InnerPageLayout.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@apollo/client";
 import Link from "next/link";
+import router, { useRouter } from "next/router";
 import React from "react";
 import { Avatar } from "../components";
 import { PROFILE_QUERY } from "../graphql";
@@ -16,36 +17,67 @@ export const InnerPageLayout = ({ children }): JSX.Element => {
   return (
     <div className={styles.wrapper}>
       <aside className={styles.aside}>
-        <Avatar
-          image={data.getProfile.avatar}
-          alt={data.getProfile.name}
-          aria-controls="simple-menu"
-        />
-        <p>{data.getProfile.name}</p>
+        <div className={styles.user}>
+          <Avatar
+            image={data.getProfile.avatar}
+            alt={data.getProfile.name}
+            aria-controls="simple-menu"
+          />
+          <p>
+            {data.getProfile.name}
+            <br />
+            R: 0 W: 0 L: 0
+          </p>
+        </div>
         <ul className={styles.navi}>
-          <li>
+          <li className={router.pathname == "/" ? styles.active : ""}>
             <Link href="/">Home</Link>
           </li>
-          <li>
+          <li
+            className={
+              router.pathname.startsWith("/users") ? styles.active : ""
+            }
+          >
             <Link href="/users">All users</Link>
           </li>
-          <li>
+          <li
+            className={
+              router.pathname.startsWith("/profile") ? styles.active : ""
+            }
+          >
             <Link href="/profile">Edit profile</Link>
           </li>
-          <li>
+          <li
+            className={router.pathname.startsWith("/game") ? styles.active : ""}
+          >
             <Link href="/game">Game</Link>
           </li>
-          <li>
+          <li
+            className={
+              router.pathname.startsWith("/chats") ? styles.active : ""
+            }
+          >
             <Link href="/chats">Chats</Link>
           </li>
-          <li>
+          <li
+            className={
+              router.pathname.startsWith("/channels") ? styles.active : ""
+            }
+          >
             <Link href="/channels">Channels</Link>
           </li>
           <li>
-            <Link href="/admin">Admin</Link>
-          </li>
-          <li>
             <Link href="/logout">Logout</Link>
+          </li>
+        </ul>
+        <hr />
+        <ul className={styles.navi}>
+          <li
+            className={
+              router.pathname.startsWith("/admins") ? styles.active : ""
+            }
+          >
+            <Link href="/admin">Admin</Link>
           </li>
         </ul>
       </aside>
