d5091235aabb36999569caa86b92cc989f54aaed
Kouichirou Sinistra <ksinistr@un-j2.kzn.21-school.ru>
2021-10-17 16:16:26 +0300

    remove unused envs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   env: {
-    API_URL: process.env.API_URL,
-    AUTH_URL: process.env.AUTH_URL,
     BACKEND_HOST: process.env.BACKEND_HOST,
     HOST: process.env.HOST,
     GAME_API_HOST: process.env.GAME_API_HOST,
